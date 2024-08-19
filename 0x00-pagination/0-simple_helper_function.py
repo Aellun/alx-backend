148c595cf8add74cbb64d419105765afe0e61077
aellun <okello.k.otieno@gmail.com>
2024-08-20 01:50:04 +0300

    pep8 corrections

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-'''function named index_range that takes two integer arguments page and page_size.
-    The function should return a tuple of size two containing a start index and an
-    end index corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
+'''function named index_range that takes two integer arguments
+    page and page_size. The function should return
+    a tuple of size two containing a start index and an
+    end index corresponding to the range of indexes
+    to return in a list for thoseparticular pagination parameters.
 '''
+
+
 def index_range(page, page_size):
     ''''
     Calculate the start and end index for pagination.
