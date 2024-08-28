7d047686b829ede22092ca1f9c2e2284c002fabb
aellun <okello.k.otieno@gmail.com>
2024-08-28 04:16:06 +0300

    added documentation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
-'''3. Parametrize templates'''
+'''3. Parametrize templates
+
+This script sets up a basic Flask application with internationalization
+support using Flask-Babel. It demonstrates how to use the `_()` function
+to parametrize templates and handle multiple languages based on the
+user's browser settings.
+
+Modules:
+    - Flask: Web framework for building the application.
+    - render_template: Function to render HTML templates.
+    - request: Object to handle incoming request data.
+    - Babel: Extension for adding i18n support to Flask.
+    - _: Alias for gettext, used for marking strings for translation.
+
+Classes:
+    - Config: class config for setting up languages, locale, and timezone.
+
+Functions:
+    - get_locale: Determines the best match for the supported languages
+      based on the request's `Accept-Language` header.
+    - index: Renders the index.html template with localized content.
+'''
+
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
 class Config:
+    '''Configuration class for Flask app.
+        Attributes:
+            LANGUAGES (list): List of supported languages.
+            BABEL_DEFAULT_LOCALE (str): Default locale for the app.
+            BABEL_DEFAULT_TIMEZONE (str): Default timezone for the app.
+    '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,11 +46,25 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    '''Select the best match for the supported languages based on the request.
+    Uses the `Accept-Language` header from the incoming request to determine
+    the best match for the supported languages defined in the Config class.
+        Returns:
+            str: The best-matching language code (e.g., 'en' or 'fr').
+    '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    '''Render the homepage with localized content.
+
+    Renders the '3-index.html' template and provides localized content for
+    the title and header based on the selected language.
+
+    Returns:
+        str: Rendered HTML content for the homepage.
+    '''
     return render_template('3-index.html',
                            home_title=_("home_title"),
                            home_header=_("home_header"))
